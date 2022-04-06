537252d6bf58d57fded75f76d4fbd4abe203d3cc
Abbas H <abbasali9960@gmail.com>
2022-04-06 19:48:45 +0530

    Added Solution Implement StrStr()

---
+++ src/LeetCodeSolPack/ImplementStrStr.java
@@ -0,0 +1,27 @@
+package LeetCodeSolPack;
+
+public class ImplementStrStr {
+	
+    public int strStr(String haystack, String needle) {
+        if(needle.isEmpty()) return 0;
+       
+          for (int i = 0; i < haystack.length(); i++) 
+          {     
+              if (haystack.startsWith(needle, i))
+                  return i;
+          }
+    return -1;
+}
+	    
+	 public static void main(String[] args)
+	 {
+		int val = 2; 
+		String s1 = "hello";
+		String s2 = "ll";
+		ImplementStrStr str = new ImplementStrStr();
+		if( val == str.strStr(s1, s2))
+			System.out.println("Success");
+		
+		
+	 }
+}
